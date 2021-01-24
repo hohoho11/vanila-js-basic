fd2c8cea83efd063a261c0cd926811dfc3afe09f
mino <ksu1346@naver.com>
2021-01-25 01:30:46 +0900

    상대

--- src/backImage.js
+++ src/backImage.js
@@ -7,7 +7,7 @@ function handleImgLoad() {
 
 function paintImage(imgNumber) {
     const image = new Image();
-    image.src = `/img/${imgNumber +1}.jpg`;
+    image.src = `img/${imgNumber +1}.jpg`;
     body.prepend(image);
     image.classList.add("bgImage");
     image.addEventListener("loadend",handleImgLoad)
